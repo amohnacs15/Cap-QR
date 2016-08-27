4ec266b189696b4431514ca7e352ba03f51b8073
BryanJonesRichardson <bryanjonesrichardson@gmail.com>
2016-08-27 16:10:31 -0400

    Showing progress spinning

--- app/src/main/java/com/androidtitan/hackathon/transaction/SendMoneyFragment.java
+++ app/src/main/java/com/androidtitan/hackathon/transaction/SendMoneyFragment.java
@@ -6,16 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.androidtitan.hackathon.App;
 import com.androidtitan.hackathon.R;
 import com.androidtitan.hackathon.myApi.model.TransferToken;
 import com.androidtitan.hackathon.server.InitiateTransferAsync;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SendMoneyFragment extends Fragment {
+
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.create_code_button)
+    Button createCodeButton;
 
     public static SendMoneyFragment newInstance() {
         return new SendMoneyFragment();
@@ -45,11 +53,13 @@ public class SendMoneyFragment extends Fragment {
     @OnClick(R.id.create_code_button)
     public void submit(View view) {
 
+        progressBar.setVisibility(View.VISIBLE);
+        createCodeButton.setVisibility(View.GONE);
+
         TransferToken token = new TransferToken();
         token.setPayer(App.payer);
         token.setAmount(100.00);
         new InitiateTransferAsync(getActivity()).execute(token);
-
 
     }
 }
